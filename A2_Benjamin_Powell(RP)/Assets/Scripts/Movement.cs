d5d93700dac1a3480f33388f8a87607312f9cf4e
BPowell97 <bennypowell127@gmail.com>
2019-03-11 17:04:22 +1100

    DoTween roate not great

--- A2_Benjamin_Powell(RP)/Assets/Scripts/Movement.cs
+++ A2_Benjamin_Powell(RP)/Assets/Scripts/Movement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Movement : MonoBehaviour
 {
@@ -13,9 +14,24 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            BarrelRoll(-1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            BarrelRoll(1);
+        }
 
         transform.Translate(speed * Input.GetAxis("Vertical") * Time.deltaTime, 0f, speed * Input.GetAxis("Horizontal") * Time.deltaTime);
 
+    }
+
+    private void BarrelRoll(int dir)
+    {
+        transform.DOMoveX(dir, 1);
+        transform.DORotate(new Vector3(0, 0, 360), 1, RotateMode.WorldAxisAdd);
     }
 
 }
